7522848c1b594c38cae979b3188ebf1768fb8934
asifthedev <asifjanii1249@gmail.com>
2024-07-06 10:55:47 +0500

    Add title of the app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 def load_model():
     with st.spinner("Downloading model..."):
         return transformers.pipeline("text-classification")
-
+st.title(":red[Text] Classification")
 input = st.text_input("Enter your text")
 
 if st.button("Classify"):
